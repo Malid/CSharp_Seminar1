17635244a5622875c8a1fd45a8efb499ed34e055
Po Lex <lehapopovn@gmail.com>
2022-08-05 14:22:14 +0300

    Task2 Ready

--- TestDZ/Program.cs
+++ TestDZ/Program.cs
@@ -7,7 +7,20 @@ void Task2()
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 {
-    System.Console.WriteLine("It works!2");
+    Clear();
+    Task2Info();
+
+    Write("Введите число 1: ");
+    int a = Convert.ToInt32(ReadLine());
+
+    Write("Введите число 2: ");
+    int b = Convert.ToInt32(ReadLine());
+    int max = 0;
+
+    if (a>b) max = a;
+    if (b>a) max = b;
+
+    WriteLine("max = " + max);
 }
 
 
@@ -17,7 +30,8 @@ void Task4()
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 {
-    System.Console.WriteLine("It works!4");
+    Clear();
+    Task4Info();
 }
 
 
@@ -44,26 +58,50 @@ void ShowMenu()
 {
     //Menu
     Clear();
+    Task2Info();
+    Task4Info();
+    Task6Info();
+    Task8Info();
+    //Menu End
+}
+
+
+void Task2Info()
+{
     WriteLine("Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
     WriteLine("a = 5; b = 7 -> max = 7");
     WriteLine("a = 2 b = 10 -> max = 10");
     WriteLine("a = -9 b = -3 -> max = -3");
     WriteLine(" ");
+}
+
+
+void Task4Info()
+{
+
     WriteLine("Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
     WriteLine("2, 3, 7 -> 7");
     WriteLine("44 5 78 -> 78");
     WriteLine("22 3 9 -> 22");
     WriteLine("");
+}
+
+
+void Task6Info()
+{
     WriteLine("Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).");
     WriteLine("4 -> да");
     WriteLine("-3 -> нет");
     WriteLine("7 -> нет");
     WriteLine("");
+}
+
+void Task8Info()
+{
     WriteLine("Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.");
     WriteLine("5 -> 2, 4");
     WriteLine("8 -> 2, 4, 6, 8");
     WriteLine("");
-    //Menu End
 }
 
 
@@ -78,8 +116,8 @@ while (Key != "q")
     {
         case "2":
             Clear();
-            //выполняем задачу 2
             Task2();
+            //выполняем задачу 2
             //WriteLine("Задача 2");
             break;
 
@@ -94,7 +132,7 @@ while (Key != "q")
             Clear();
             Task6();
             //выполняем задачу 6
-            //WriteLine("Задача 6");
+            WriteLine("Задача 6");
             break;
 
         case "8":
